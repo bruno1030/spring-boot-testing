60b3ce5c944a5b52765c3d33ffa0908705e627fb
Bruno <brunomartins1030@gmail.com>
2022-12-16 14:29:14 -0300

    test for saveEmployee method which throws Exception

--- src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTests.java
+++ src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTests.java
@@ -1,13 +1,22 @@
 package net.javaguides.springboottesting.service;
 
+import net.javaguides.springboottesting.exception.ResourceNotFoundException;
 import net.javaguides.springboottesting.model.Employee;
 import net.javaguides.springboottesting.repository.EmployeeRepository;
 import net.javaguides.springboottesting.service.impl.EmployeeServiceImpl;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -37,6 +46,7 @@ public class EmployeeServiceTests {
     }
 
     // JUnit test for saveEmployee method
+    @DisplayName("JUnit test for saveEmployee method")
     @Test
     public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject(){
         // given - precondition of setup
@@ -54,6 +64,28 @@ public class EmployeeServiceTests {
 
         // then - verify the output
         assertThat(savedEmployee.getFirstName()).isEqualTo("Bruno");
+    }
+
+    // JUnit test for saveEmployee method
+    @DisplayName("JUnit test for saveEmployee method which throws exception")
+    @Test
+    public void givenExistingEmail_whenSaveEmployee_thenThrowsException(){
+        // given - precondition of setup
+        // returning empty so then it will be possible to test the saveEmployee method
+        given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.of(employee));
+
+        //testing the save method ... nesse caso nao preciso testar o save pois o findByEmail retornou um employee, portanto nao irei salvar novamente no banco com o mesmo email, irei apenas jogar a excecao
+        //given(employeeRepository.save(employee)).willReturn(employee);
+
+        // when - action or the behaviour that we are going test
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            employeeService.saveEmployee(employee);
+        });
+
+        // then
+        verify(employeeRepository, never()).save(any(Employee.class));
+
     }
 
 }
